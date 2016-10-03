e2ae15a29837464a500d4be51fc4325a5315c35c
Pablo Cleij <pablocley@hotmail.com>
2016-10-03 02:11:27 +0200

    Initial commit

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Zoeken naar:', 'label', 'Biergilde Groningen' ) ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Zoeken...', 'jointswp' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Zoeken naar:', 'Biergilde' ) ?>" />
+	</label>
+	<input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Zoeken', 'Biergilde Groningen' ) ?>" />
+</form>
